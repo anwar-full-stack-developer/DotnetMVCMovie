833002397788b45c6d1531bd75ff50d093489826
Anwar <ranaars@gmail.com>
2022-12-05 17:32:50 +0600

    First Commit Asp.Net Core Movie App
    First Commit Asp.Net Core Movie App

---
+++ DotnetMVCMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetMVCMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
